98d06903d83a42bd4fcbb46cac6385d36808003c
vilas-123 <145769936+vilas-123@users.noreply.github.com>
2026-03-20 17:12:25 +0530

    Refactor tests by removing old tests and adding new one
    Removed old tests and added a new test for homepage status code.

--- myappdevops/tests.py
+++ myappdevops/tests.py
@@ -1,28 +1,10 @@
-from django.test import TestCase
-from django.urls import reverse
-import os
+# myappdevops/tests.py
+from django.test import TestCase, Client
 
+class WelcomePageTest(TestCase):
+    def setUp(self):
+        self.client = Client()
 
-class HomePageTest(TestCase):
-
-    def test_home_page_status_code(self):
-        url = reverse('home')
-        response = self.client.get(url)
+    def test_homepage_returns_200(self):
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-
-    def test_home_page_content(self):
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertContains(response, "Welcome to my Django app")
-
-
-class JenkinsLearningTest(TestCase):
-
-    def test_conditional_fail(self):
-        """
-        This test will FAIL only if environment variable is set
-        """
-        if os.getenv("FAIL_TEST") == "1":
-            self.assertEqual(1, 2)  # ❌ force fail
-        else:
-            self.assertEqual(1, 1)  # ✅ pass
